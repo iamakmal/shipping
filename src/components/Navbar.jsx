b04ffbd70c4c6d539132a247b9db1d0b1bc22daf
iamakmal <m.akmalalavi@gmail.com>
2024-04-05 14:27:46 +0530

    implemented nav bar

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,75 @@
+import React from 'react'
+
+export default function Navbar() {
+    return (
+        <>
+        <div className="navbar bg-base-100">
+  <div className="navbar-start">
+    <div className="dropdown">
+      <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
+      </div>
+      <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
+        <li><a>Item 1</a></li>
+        <li>
+          <a>Parent</a>
+          <ul className="p-2">
+            <li><a>Submenu 1</a></li>
+            <li><a>Submenu 2</a></li>
+          </ul>
+        </li>
+        <li><a>Item 3</a></li>
+      </ul>
+    </div>
+    <img className="h-12 w-auto px-10" src="/logo.png" alt="logo"/>
+  </div>
+  <div className="navbar-center hidden lg:flex">
+    <ul className="menu menu-horizontal px-1">
+      <li>
+        <details>
+          <summary className='text-lg font-medium'>Home</summary>
+          <ul className="p-2">
+            <li><a>Submenu 1</a></li>
+            <li><a>Submenu 2</a></li>
+          </ul>
+        </details>
+      </li>
+      <li>
+        <details>
+          <summary className='text-lg font-medium'>Pages</summary>
+          <ul className="p-2">
+            <li><a>Submenu 1</a></li>
+            <li><a>Submenu 2</a></li>
+          </ul>
+        </details>
+      </li>
+      <li><a className='text-lg font-medium'>Tracking</a></li>
+      <li>
+        <details>
+          <summary className='text-lg font-medium'>Services</summary>
+          <ul className="p-2">
+            <li><a>Submenu 1</a></li>
+            <li><a>Submenu 2</a></li>
+          </ul>
+        </details>
+      </li>
+      <li>
+        <details>
+          <summary className='text-lg font-medium'>Blog</summary>
+          <ul className="p-2">
+            <li><a>Submenu 1</a></li>
+            <li><a>Submenu 2</a></li>
+          </ul>
+        </details>
+      </li>
+      
+    </ul>
+  </div>
+  <div className="navbar-end">
+    <a className="btn btn-warning text-white rounded-none">GET A QUOTE</a>
+    <a className="btn bg-[#171717] mx-8 text-white rounded-none">SIGN IN</a>
+  </div>
+</div>
+        </>
+    )
+}
